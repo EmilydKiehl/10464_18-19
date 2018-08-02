4d5a49a3f6f670bdb0d1357d3204d68f52c8df89
ADMIN <qcjames53@gmail.com>
2018-08-01 20:43:29 -0700

    Added vuforia sample code into class and created Vuforia objects online

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavVuforia.java
@@ -20,7 +20,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import java.util.ArrayList;
 import java.util.List;
 
-@Autonomous(name="Concept: Vuforia Navigation", group ="Concept")
+@Autonomous(name="Vuforia Distance Tracker", group ="summerProjects")
 public class NavVuforia extends LinearOpMode {
 
     public static final String TAG = "Vuforia Navigation Sample";
@@ -34,51 +34,26 @@ public class NavVuforia extends LinearOpMode {
     VuforiaLocalizer vuforia;
 
     @Override public void runOpMode() {
-        /*
-         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
-         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
-         */
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        // OR...  Do Not Activate the Camera Monitor View, to save power
-        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        /*
-         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
-         * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
-         * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
-         * web site at https://developer.vuforia.com/license-manager.
-         *
-         * Vuforia license keys are always 380 characters long, and look as if they contain mostly
-         * random data. As an example, here is a example of a fragment of a valid key:
-         *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
-         * Once you've obtained a license key, copy the string from the Vuforia web site
-         * and paste it in to your code onthe next line, between the double quotes.
-         */
         parameters.vuforiaLicenseKey = " AYSaZfX/////AAABGZyGj0QLiEYhuyrGuO59xV2Jyg9I+WGlfjyEbBxExILR4A183M1WUKucNHp5CnSpDGX5nQ9OD3w5WCfsJuudFyJIJSKZghM+dOlhTWWcEEGk/YB0aOLEJXKK712HpyZqrvwpXOyKDUwIZc1mjWyLT3ZfCmNHQ+ouLKNzOp2U4hRqjbdWf1ZkSlTieiR76IbF6x7MX5ZtRjkWeLR5hWocakIaH/ZPDnqo2A2mIzAzCUa8GCjr80FJzgS9dD77lyoHkJZ/5rNe0k/3HfUZXA+BFSthRrtai1W2/3oRCFmTJekrueYBjM4wuuB5CRqCs4MG/64AzyKOdqmI05YhC1tVa2Vd6Bye1PaMBHmWNfD+5Leq ";
 
-        /*
-         * We also indicate which camera on the RC that we wish to use.
-         * Here we chose the back (HiRes) camera (for greater range), but
-         * for a competition robot, the front camera might be more convenient.
-         */
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
         VuforiaTrackables flag_finder = this.vuforia.loadTrackablesFromAsset("flag_finder");
-        VuforiaTrackable trendy  = flag_finder.get(2);
+        VuforiaTrackable trendy  = flag_finder.get(0);
         trendy.setName("Trendy");
 
-        /** For convenience, gather together all the trackable objects in one easily-iterable collection */
         List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
-        allTrackables.addAll(flag_finder);
+        allTrackables.add(flag_finder.get(0));
         trendy.setLocation(new OpenGLMatrix());
 
         telemetry.addData(">", "Press Play to see if this bad boy works...");
         telemetry.update();
         waitForStart();
 
-        /** Start tracking the data sets we care about. */
+       //activate vuforia
         flag_finder.activate();
 
         while (opModeIsActive()) {
@@ -90,8 +65,15 @@ public class NavVuforia extends LinearOpMode {
                  * getRobotLocation() will return null if the trackable is not currently visible.
                  */
                 telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
+                OpenGLMatrix testLocation = ((VuforiaTrackableDefaultListener) trackable.getListener()).getPose();
+                String outputString = "NULL";
+                if(testLocation != null) {
+                    outputString = format(trackable.getLocation());
+                }
 
+                telemetry.addData("P-" + trackable.getName(), outputString);
                 OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+
                 if (robotLocationTransform != null) {
                     lastLocation = robotLocationTransform;
                 }
